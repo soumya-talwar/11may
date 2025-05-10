06793f2505ca9c6b2f0a06e0d682b30900e23c89
Soumya Talwar <soumyatalwar@Soumyas-MacBook-Pro.local>
2025-05-10 13:16:53 +0530

    image test

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-import OpenAI from "openai";
 import "dotenv/config";
 
+import OpenAI from "openai";
 const openai = new OpenAI({
 	apiKey: process.env.APIKEY,
 });
 
-const completion = openai.chat.completions.create({
-	model: "gpt-4o-mini",
-	store: true,
-	messages: [
-		{ role: "user", content: "give me a short compliment on my birthday" },
-	],
-});
+import twilio from "twilio";
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
 
-completion.then((result) => console.log(result.choices[0].message.content));
+function compliment() {
+	openai.chat.completions
+		.create({
+			model: "gpt-4o-mini",
+			store: true,
+			messages: [
+				{
+					role: "user",
+					content: "give me a short compliment on my birthday",
+				},
+			],
+		})
+		.then(async (output) => {
+			await client.messages.create({
+				body: output.choices[0].message.content,
+				from: "whatsapp:+14155238886",
+				mediaUrl: "/data/images/test.png",
+				to: `whatsapp:+91${process.env.PHONENUMBER}`,
+			});
+			console.log("complimented!");
+			console.log("compliment: " + output.choices[0].message.content);
+		});
+}
+
+let interval = 1000 * 5; // 5 seconds
+// let interval = 1000 * 60 * 30; // 30 minutes
+
+let birthday = setInterval(() => {
+	let date = new Date();
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	if (day == 10 && month == 5) compliment();
+	else if (day == 12) {
+		console.log("shutting down!");
+		clearInterval(birthday);
+	}
+}, interval);
