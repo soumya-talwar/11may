fb8b3e5741ff4c98e8be4879d7c89c7054fcf0ce
Soumya Talwar <soumyatalwar@Soumyas-MacBook-Pro.local>
2025-05-12 01:19:54 +0530

    hidden data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import OpenAI from "openai";
 const openai = new OpenAI({
-	apiKey: process.env.APIKEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
 import twilio from "twilio";
@@ -10,28 +10,14 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-import express from "express";
-const app = express();
-const port = process.env.PORT || 10000;
-
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
-
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
-
 let wins;
-let interval = 1000 * 60 * 30;
+let interval = 1000 * 60 * 60;
 
 fetch(
-	"https://raw.githubusercontent.com/soumya-talwar/may11/refs/heads/main/data/wins.json"
+	`https://raw.githubusercontent.com/soumya-talwar/host/refs/heads/main/wins.json?token=${process.env.GITHUB_TOKEN}`
 )
 	.then((response) => response.json())
-	.then((data) => {
-		wins = data.wins;
-	});
+	.then((data) => (wins = data.wins));
 
 function compliment() {
 	let win = wins[Math.floor(Math.random(wins.length) * wins.length)];
@@ -57,13 +43,13 @@ function compliment() {
 					body: output.choices[0].message.content,
 					from: "whatsapp:+14155238886",
 					mediaUrl: `https://raw.githubusercontent.com/soumya-talwar/may11/refs/heads/main/data/images/${win.image}`,
-					to: `whatsapp:+91${process.env.PHONENUMBER}`,
+					to: `whatsapp:+91${process.env.PHONE_NUMBER}`,
 				};
 			else
 				params = {
 					body: output.choices[0].message.content,
 					from: "whatsapp:+14155238886",
-					to: `whatsapp:+91${process.env.PHONENUMBER}`,
+					to: `whatsapp:+91${process.env.PHONE_NUMBER}`,
 				};
 			await client.messages.create(params);
 			console.log("complimented!");
@@ -75,7 +61,7 @@ let start = setInterval(() => {
 	let date = new Date();
 	let day = date.getDate();
 	let month = date.getMonth() + 1;
-	if ((day == 11 || day == 10) && month == 5) compliment();
+	if (day == 11 && month == 5) compliment();
 	else if (day == 12) {
 		console.log("shutting down!");
 		clearInterval(start);
